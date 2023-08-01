3543dd90cbb6f5aa838fb0c15e54cd906c566290
minkee009 <minkee5004@gmail.com>
2023-08-01 11:46:28 +0900

    playerPositionClamp
    플레이어 위치 제한 (맵 이탈 방지)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using Unity.VisualScripting;
+using UnityEditor.Build;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -28,13 +29,30 @@ public class PlayerController : MonoBehaviour
         var inputX = (Input.GetKey(KeyCode.RightArrow) ? 1 : 0) + (Input.GetKey(KeyCode.LeftArrow) ? -1 : 0);
         var inputY = (Input.GetKey(KeyCode.UpArrow) ? 1 : 0) + (Input.GetKey(KeyCode.DownArrow) ? -1 : 0);
 
+        var wishPos = new Vector3(inputX * 2f, inputY, 0) * 4f * Time.deltaTime;
 
-        targetRotY = inputX != 0 
+        if (Mathf.Abs((transform.position + wishPos).x) > 65f)
+        {
+            var clampF = wishPos.x > 0 ? 1 : -1;
+            transform.position = new Vector3(65f * clampF, transform.position.y, transform.position.z);
+            inputX = 0;
+            wishPos.x = 0;
+        }
+
+        if (Mathf.Abs((transform.position + wishPos).y) > 40f)
+        {
+            var clampF = wishPos.y > 0 ? 1 : -1;
+            transform.position = new Vector3(transform.position.x, 40f * clampF, transform.position.z);
+            wishPos.y = 0;
+        }
+
+        targetRotY = inputX != 0
             ? (inputX > 0 ? -rotateAmount : rotateAmount) : 0;
 
         currentRotY = Mathf.Lerp(currentRotY, targetRotY, 25f * Time.deltaTime);
 
-        transform.Translate(new Vector3(inputX, inputY, 0) * 4f * Time.deltaTime);
+
+        transform.Translate(wishPos);
         rootTransform.localRotation = Quaternion.Euler(0f, currentRotY, 0f);
 
         if (Input.GetKeyDown(KeyCode.C))
