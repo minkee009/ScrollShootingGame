ebff27ddd73fa15fa57e4027d60f060527d6dbd6
minkee009 <minkee5004@gmail.com>
2023-08-17 08:32:17 +0900

    refresh_SSSM_10_h
    플레이어 사망 후 드론 봇 버그 해결

--- Assets/Scripts/InGameObj/DroneBot/BotMove.cs
+++ Assets/Scripts/InGameObj/DroneBot/BotMove.cs
@@ -15,7 +15,7 @@ public class BotMove : MonoBehaviour
     void Update()
     {
         if (GameManager.instance.currentGameState != CurrentGameState.Play 
-            || ReferenceEquals(Center, null)) return;
+            || Center == null) return;
         _angle += speed * Time.deltaTime * GameManager.instance.inGameTimeSpeed;
         var targetPos = new Vector3(Mathf.Cos(_angle), Mathf.Sin(_angle), -0.2f) * radius + Center.position;
         transform.position = Vector3.Lerp(transform.position,targetPos, 6f * Time.deltaTime);
